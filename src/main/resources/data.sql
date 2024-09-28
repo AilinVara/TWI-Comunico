a75950ebb19665d71223939a5ba9b73738215183
JulianImperiale <julian.m.imperiale@gmail.com>
2024-09-28 18:33:37 -0300

    Se agregó el CSS correspondiente para las lecciones(Sujeto a cambios) y todas las imagenes del abecedario de braille a la carpeta de img.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@ INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.e
 
 INSERT INTO Opcion(id, descripcion) VALUES (1, 'A'), (2, 'B'), (3, 'C');
 
-INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id) VALUES (1, 'Ejercicio de traduccion: ', 1);
+INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id) VALUES (1, '¿Cuál es la letra que aparece en la siguiente imagen? ', 1);
 
 INSERT INTO Ejercicio_Opcion(Ejercicio_id, opcionesIncorrectas_id) VALUES (1,2), (1,3);
+
