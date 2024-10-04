e703e2b8be47d7c365e51c69054fa20181219b03
Lira <ailinvara@gmail.com>
2024-10-04 14:57:39 -0300

    Actualiza atributos de la entidad curso

--- src/main/java/com/tallerwebi/dominio/Curso.java
+++ src/main/java/com/tallerwebi/dominio/Curso.java
@@ -32,4 +32,89 @@ public class Curso {
     public enum Nivel {
         BÁSICO, INTERMEDIO, AVANZADO
     }
+
+    public Curso() {}
+
+    public Curso(String nombre, String descripcion, LocalDate fecha, LocalTime hora, Tipo tipo, Nivel nivel, int capacidad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.tipo = tipo;
+        this.nivel = nivel;
+        this.capacidad = capacidad;
+        this.inscriptos = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Nivel getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Nivel nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getInscriptos() {
+        return inscriptos;
+    }
+
+    public void setInscriptos(int inscriptos) {
+        this.inscriptos = inscriptos;
+    }
 }
