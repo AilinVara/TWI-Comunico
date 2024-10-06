076b7c5ad5303aa84d66fa8af0f510b50d136d2c
Lira <ailinvara@gmail.com>
2024-10-05 21:27:42 -0300

    Agrega método buscarCursosPorNombre

--- src/main/java/com/tallerwebi/dominio/ServicioCursoImpl.java
+++ src/main/java/com/tallerwebi/dominio/ServicioCursoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service("servicioCurso")
 @Transactional
@@ -26,6 +27,17 @@ public class ServicioCursoImpl implements ServicioCurso {
     @Override
     public List<Curso> ordenarCursosPorFecha(String ordenFecha, List<Curso> cursosFiltrados) {
         return repositorioCurso.ordenarCursosPorFecha(ordenFecha, cursosFiltrados);
+    }
+
+    @Override
+    public List<Curso> buscarCursosPorNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            return obtenerCursosDisponibles();
+        }
+        List<Curso> cursos = repositorioCurso.obtenerTodosLosCursos();
+        return cursos.stream()
+                .filter(curso -> curso.getNombre().toLowerCase().contains(nombre.toLowerCase()))
+                .collect(Collectors.toList());
     }
 
 }
