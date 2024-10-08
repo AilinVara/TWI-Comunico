3e795d19ee951012dae9ba4b0fd0aadf2bdfb4f3
Lira <ailinvara@gmail.com>
2024-10-08 19:43:12 -0300

    Refactoriza ejercicio-forma-palabra

---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioEjercicioFormaPalabraImpl.java
@@ -0,0 +1,7 @@
+package com.tallerwebi.infraestructura;
+
+import com.tallerwebi.dominio.RepositorioEjercicioFormaPalabra;
+
+public class RepositorioEjercicioFormaPalabraImpl implements RepositorioEjercicioFormaPalabra {
+
+}
