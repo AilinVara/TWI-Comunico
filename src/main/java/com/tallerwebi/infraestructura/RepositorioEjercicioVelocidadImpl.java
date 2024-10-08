20fa51cb44a0a5ad46aad4e83e1c804020ce4fe3
Lira <ailinvara@gmail.com>
2024-10-08 13:52:13 -0300

    Agrega vista de ejercicio-velocidad

---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioEjercicioVelocidadImpl.java
@@ -0,0 +1,7 @@
+package com.tallerwebi.infraestructura;
+
+import com.tallerwebi.dominio.RepositorioEjercicioVelocidad;
+
+public class RepositorioEjercicioVelocidadImpl implements RepositorioEjercicioVelocidad {
+
+}
