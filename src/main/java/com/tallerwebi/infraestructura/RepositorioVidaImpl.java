cbf4c69a9824a7950eb68d28f68d44abf75ed3bc
JulianImperiale <julian.m.imperiale@gmail.com>
2024-10-09 10:30:09 -0300

    Se agregaron las clases e interfaces necesarias para la funcionalidad de la vida del usuario.

---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioVidaImpl.java
@@ -0,0 +1,37 @@
+package com.tallerwebi.infraestructura;
+
+import com.tallerwebi.dominio.RepositorioVida;
+import com.tallerwebi.dominio.Vida;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository("repositorioVida")
+public class RepositorioVidaImpl implements RepositorioVida {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public RepositorioVidaImpl (SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void guardarUnaVida(Vida vida) {
+        this.sessionFactory.getCurrentSession().save(vida);
+    }
+
+    @Override
+    public Vida buscarUnaVidaPorId(Long id) {
+        final Session session = sessionFactory.getCurrentSession();
+        return (Vida) session.createCriteria(Vida.class)
+                .add(Restrictions.eq("id", id))
+                .uniqueResult();
+    }
+
+
+}
