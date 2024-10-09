403afa0eb5ef7878b838b2b12041c144e6750726
ricartes123 <pedro-le2003@hotmail.com>
2024-10-09 14:03:54 -0300

    Agrega datos de ejemplo para Matriz

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,20 @@
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+INSERT INTO Usuario(id, email, password, rol, activo) VALUES
+(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 
-INSERT INTO Opcion(id, descripcion) VALUES (1, 'A'), (2, 'B'), (3, 'C');
+INSERT INTO Opcion(id, descripcion) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C');
 
-INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id) VALUES (1, 'Ejercicio de traduccion: ', 1);
+INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id) VALUES
+(1, 'Ejercicio de traduccion: ', 1),
+(10, 'Forma la letra: A ', 1),
+(11, 'Forma la letra: B ', 2),
+(12, 'Forma la letra: C ', 3);
 
-INSERT INTO Ejercicio_Opcion(Ejercicio_id, opcionesIncorrectas_id) VALUES (1,2), (1,3);
+INSERT INTO Ejercicio_Opcion(Ejercicio_id, opcionesIncorrectas_id) VALUES
+(1,2),
+(1,3);
 
 INSERT INTO Letra(id, nombre, imagenSenias, imagenBraille) VALUES
 (1, "A", 'senias-a.png', 'braille-a.png'),
@@ -34,3 +44,10 @@ INSERT INTO Letra(id, nombre, imagenSenias, imagenBraille) VALUES
 (25, "X", 'senias-x.png', 'braille-x.png'),
 (26, "Y", 'senias-y.png', 'braille-y.png'),
 (27, "Z", 'senias-z.png', 'braille-z.png');
+
+INSERT INTO Matriz (id, ejercicio_id, puntos) VALUES
+(1, 10, '100000'),
+(2, 11, '101000'),
+(3, 12, '110000');
+
+
