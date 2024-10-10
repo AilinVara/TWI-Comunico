f5e26cd390c5b8acabe5f8a1acbecc13616ce202
ricartes123 <pedro-le2003@hotmail.com>
2024-10-09 23:51:46 -0300

    Inserta ejemplos de Leccion, Ejercicio y Matriz para formaLetra

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,33 @@
-INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+INSERT INTO Usuario(id, email, password, rol, activo) VALUES
+(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 
-INSERT INTO Opcion(id, descripcion) VALUES (1, 'A'), (2, 'B'), (3, 'C');
+INSERT INTO Opcion(id, descripcion) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C');
 
-INSERT INTO Leccion(id, titulo) VALUES (1, 'leccion 1:');
+INSERT INTO Leccion(id, titulo) VALUES
+(1, 'leccion 1:'),
+(2, 'leccion 2:'),
+(3, 'leccion 3:');
 
-INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id, leccion_id) VALUES (1, 'Ejercicio 1: ', 1, 1), (2, 'Ejercicio 2:', 2, 1), (3, 'Ejercicio 3:', 3, 1);
+INSERT INTO Ejercicio(id, consigna, opcionCorrecta_id, leccion_id) VALUES
+(1, 'Ejercicio 1:', 1, 1),
+(2, 'Ejercicio 2:', 2, 1),
+(3, 'Ejercicio 3:', 3, 1),
+(10, 'Forma la letra: A', 1, 2),
+(11, 'Forma la letra: B', 2, 2),
+(12, 'Forma la letra: C', 3, 2);
 
-INSERT INTO Ejercicio_Opcion(Ejercicio_id, opcionesIncorrectas_id) VALUES (1,2), (1,3), (2, 1), (2, 3), (3, 1), (3, 2);
+INSERT INTO Ejercicio_Opcion(Ejercicio_id, opcionesIncorrectas_id) VALUES
+(1,2),
+(1,3),
+(2, 1),
+(2, 3),
+(3, 1),
+(3, 2);
+
+INSERT INTO Matriz (id, ejercicio_id, puntos) VALUES
+(1, 10, '100000'),
+(2, 11, '101000'),
+(3, 12, '110000');
