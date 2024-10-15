72d2a093a990f024ccd08196f6874cedfe304ed2
JulianImperiale <julian.m.imperiale@gmail.com>
2024-10-15 10:51:13 -0300

    Revert "Se agregaron las clases necesarias para el sistema de logros y sus condiciones, tambien sus clase servicio y repositorio"
    This reverts commit d2ed03e18a328f727632843f1f94b8d004edbe3b.

--- src/main/java/com/tallerwebi/dominio/ServicioCondicionLogro.java
+++
@@ -1,7 +0,0 @@
-package com.tallerwebi.dominio;
-
-public interface ServicioCondicionLogro {
-    void agregarCondicion(CondicionLogro condicionLogro);
-    void guardar(CondicionLogro condicionLogro);
-    CondicionLogro buscarCondicionLogroPorSuId(Long id);
-}
--- src/main/java/com/tallerwebi/dominio/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/Usuario.java
@@ -1,7 +1,9 @@
 package com.tallerwebi.dominio;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Usuario {
@@ -13,9 +15,6 @@ public class Usuario {
     private String password;
     private String rol;
     private Boolean activo = false;
-
-   @OneToOne
-    private Set<Logro> logros;
 
     public Long getId() {
         return id;
@@ -46,14 +45,6 @@ public class Usuario {
     }
     public void setActivo(Boolean activo) {
         this.activo = activo;
-    }
-
-    public Set<Logro> getRangos() {
-        return logros;
-    }
-
-    public void setRangos(Set<Logro> logroes) {
-        this.logros = logroes;
     }
 
     public boolean activo() {
