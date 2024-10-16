b201a24cc19ac44d8b034b4524427db0a0d3bbc0
JulianImperiale <julian.m.imperiale@gmail.com>
2024-10-16 12:43:29 -0300

    Ahora el usuario puede tocar en el boton de sus vidas para ver cuantas vidas le quedan y cuanto falta para su próxima vida. Solo fue implementado en el ControladorEjercicio, en el ejercicio de braille en su respectivo HTMLy CSS. También se agregaron todos los CSS para este sistema de vidas.

--- src/main/java/com/tallerwebi/presentacion/ControladorEjercicio.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorEjercicio.java
@@ -33,7 +33,7 @@ public class ControladorEjercicio {
 
 
     @RequestMapping(value = "/ejercicio/{indice}", method = RequestMethod.GET)
-    public ModelAndView irAjercicio(@RequestParam("leccion") Long leccionId, @PathVariable("indice") Integer indice, HttpServletRequest request){
+    public ModelAndView irAjercicio(@RequestParam("leccion") Long leccionId, @PathVariable("indice") Integer indice, HttpServletRequest request) {
         ModelMap modelo = new ModelMap();
         modelo.put("leccion", leccionId);
         Long usuarioId = (Long) request.getSession().getAttribute("id");
@@ -42,25 +42,26 @@ public class ControladorEjercicio {
         modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
         modelo.put("ejercicio", ejercicio);
         modelo.put("indice", indice);
+        agregarTiempoRestanteAlModelo(modelo,usuarioId);
 
-        if(ejercicio.getId() >= 10){
+        if (ejercicio.getId() >= 10) {
             Matriz matriz;
             matriz = this.servicioMatriz.obtenerMatrizPorEjercicio(ejercicio.getId());
-            modelo.put("matriz",matriz);
+            modelo.put("matriz", matriz);
             return new ModelAndView("formaLetras", modelo);
         }
         return new ModelAndView("ejercicio", modelo);
     }
 
-    @RequestMapping( path = "/resolver/{indice}", method = RequestMethod.POST)
-    public ModelAndView resolverEjercicio(@PathVariable("indice") Long indice, @RequestParam("opcionSeleccionada") Long opcionId, @RequestParam("ejercicioId")Long ejercicioId,
-                                          @RequestParam("leccion")Long leccionId, HttpServletRequest request){
+    @RequestMapping(path = "/resolver/{indice}", method = RequestMethod.POST)
+    public ModelAndView resolverEjercicio(@PathVariable("indice") Long indice, @RequestParam("opcionSeleccionada") Long opcionId, @RequestParam("ejercicioId") Long ejercicioId,
+                                          @RequestParam("leccion") Long leccionId, HttpServletRequest request) {
         ModelMap modelo = new ModelMap();
         Ejercicio ejercicio = this.servicioEjercicio.obtenerEjercicio(ejercicioId);
         Long usuarioId = (Long) request.getSession().getAttribute("id");
         ProgresoLeccion progreso = this.servicioProgresoLeccion.buscarPorIds(leccionId, usuarioId, ejercicioId);
 
-        if(progreso == null){
+        if (progreso == null) {
             this.servicioProgresoLeccion.crearProgresoLeccion(leccionId, usuarioId);
             progreso = this.servicioProgresoLeccion.buscarPorIds(leccionId, usuarioId, ejercicioId);
         }
@@ -68,34 +69,27 @@ public class ControladorEjercicio {
         this.servicioProgresoLeccion.actualizarProgreso(progreso, resuelto);
         modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
 
-        if (!resuelto){
+        if (!resuelto) {
             this.servicioVida.perderUnaVida(usuarioId);
             modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
-
-            Vida vida = this.servicioVida.obtenerVida(usuarioId);
-            LocalDateTime ahora = LocalDateTime.now();
-            Duration duracion = Duration.between(vida.getUltimaVezQueSeRegeneroLaVida(), ahora);
-            long segundosDesdeUltimaRegeneracion = duracion.getSeconds();
-            long tiempoRestante = 60 - (segundosDesdeUltimaRegeneracion % 60); // Cada 60 segundos
-            modelo.put("tiempoRestante", tiempoRestante);
         }
 
-
+        agregarTiempoRestanteAlModelo(modelo,usuarioId);
 
         modelo.put("indice", indice);
         modelo.put("leccion", leccionId);
-        modelo.put("ejercicio",ejercicio);
+        modelo.put("ejercicio", ejercicio);
         modelo.put("esCorrecta", (resuelto));
         return new ModelAndView("ejercicio", modelo);
     }
 
     @RequestMapping(path = "/resolverMatriz/{indice}", method = RequestMethod.POST)
     public ModelAndView resolverMatriz(@PathVariable("indice") Long indice,
-                                       @RequestParam("ejercicioId")Long ejercicioId,
-                                       @RequestParam("leccion")Long leccionId,
-                                       @RequestParam("matrizId")Long matrizId,
+                                       @RequestParam("ejercicioId") Long ejercicioId,
+                                       @RequestParam("leccion") Long leccionId,
+                                       @RequestParam("matrizId") Long matrizId,
                                        @RequestParam("puntosSeleccionados") String puntosSeleccionados,
-                                       HttpServletRequest request){
+                                       HttpServletRequest request) {
 
         ModelMap modelo = new ModelMap();
         Ejercicio ejercicio = this.servicioEjercicio.obtenerEjercicio(ejercicioId);
@@ -104,7 +98,7 @@ public class ControladorEjercicio {
         Long usuarioId = (Long) request.getSession().getAttribute("id");
         ProgresoLeccion progreso = this.servicioProgresoLeccion.buscarPorIds(leccionId, usuarioId, ejercicioId);
 
-        if(progreso == null){
+        if (progreso == null) {
             this.servicioProgresoLeccion.crearProgresoLeccion(leccionId, usuarioId);
             progreso = this.servicioProgresoLeccion.buscarPorIds(leccionId, usuarioId, ejercicioId);
         }
@@ -133,11 +127,13 @@ public class ControladorEjercicio {
             modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
         }
 
+        agregarTiempoRestanteAlModelo(modelo,usuarioId);
         modelo.put("indice", indice);
         return new ModelAndView("formaLetras", modelo);
     }
+
     @RequestMapping(value = "/ejercicio-video", method = RequestMethod.GET)
-    public ModelAndView irAEjercicioVideo(@RequestParam(required = false, defaultValue = "2", value = "id") Long id){
+    public ModelAndView irAEjercicioVideo(@RequestParam(required = false, defaultValue = "2", value = "id") Long id) {
         ModelMap modelo = new ModelMap();
         Ejercicio ejercicioVideo = servicioEjercicio.obtenerEjercicio(id);
         modelo.put("ejercicio", ejercicioVideo);
@@ -145,21 +141,30 @@ public class ControladorEjercicio {
     }
 
     @RequestMapping(path = "/resolverVideo", method = RequestMethod.POST)
-    public ModelAndView resolverEjercicioVideo(@RequestParam("opcionSeleccionada") Long opcionId,@RequestParam("ejercicioId")Long ejercicioId, HttpServletRequest request){
+    public ModelAndView resolverEjercicioVideo(@RequestParam("opcionSeleccionada") Long opcionId, @RequestParam("ejercicioId") Long ejercicioId, HttpServletRequest request) {
         ModelMap modelo = new ModelMap();
         Ejercicio ejercicioVideo = this.servicioEjercicio.obtenerEjercicio(ejercicioId);
         Long usuarioId = (Long) request.getSession().getAttribute("id");
         Boolean resuelto = this.servicioEjercicio.resolverEjercicio(ejercicioVideo, opcionId);
         modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
 
-        if (!resuelto){
+        if (!resuelto) {
             this.servicioVida.perderUnaVida(usuarioId);
             modelo.put("vidas", this.servicioVida.obtenerVida(usuarioId).getCantidadDeVidasActuales());
         }
-        modelo.put("ejercicio",ejercicioVideo);
+        modelo.put("ejercicio", ejercicioVideo);
         modelo.put("esCorrecta", (resuelto));
         return new ModelAndView("ejercicio-video", modelo);
     }
 
+    private void agregarTiempoRestanteAlModelo(ModelMap modelo, Long usuarioId) {
+        Vida vida = this.servicioVida.obtenerVida(usuarioId);
+        LocalDateTime ahora = LocalDateTime.now();
+        Duration duracion = Duration.between(vida.getUltimaVezQueSeRegeneroLaVida(), ahora);
+        long segundosDesdeUltimaRegeneracion = duracion.getSeconds();
+        long tiempoRestante = 60 - (segundosDesdeUltimaRegeneracion % 60); // Cada 60 segundos
+        modelo.put("tiempoRestante", tiempoRestante);
+    }
 
 }
+
