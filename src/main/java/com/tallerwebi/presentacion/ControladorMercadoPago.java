1399919ca89de859fda33f87baaa57358434357c
maertin <mutuverria00gmail.com>
2024-10-23 14:14:52 -0300

    Crea la vista para realizar la compra por MercadoPago, el ControladorMercadoPago para manejar las requests y el json que crea el botón de compra

--- src/main/java/com/tallerwebi/presentacion/ControladorMercadoPago.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorMercadoPago.java
@@ -1,12 +1,16 @@
 package com.tallerwebi.presentacion;
 
 import com.mercadopago.MercadoPagoConfig;
-import com.mercadopago.client.common.IdentificationRequest;
-import com.mercadopago.client.payment.PaymentClient;
-import com.mercadopago.client.payment.PaymentCreateRequest;
-import com.mercadopago.client.payment.PaymentPayerRequest;
-import com.mercadopago.core.MPRequestOptions;
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
+import com.mercadopago.client.preference.PreferenceClient;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.resources.preference.Preference;
+import com.tallerwebi.dominio.ServicioUsuario;
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,67 +18,81 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
 
 @Controller
 public class ControladorMercadoPago {
+
     @Value("${codigo.mercadoLibre}")
     private String mercadoLibreToken;
 
-    public ControladorMercadoPago() {
+    private ServicioUsuario servicioUsuario;
 
+    @Autowired
+    public ControladorMercadoPago(ServicioUsuario servicioUsuario) {
+        this.servicioUsuario = servicioUsuario;
     }
 
     @RequestMapping(value = "/mp/{cantidad}", method = RequestMethod.POST)
-    public ResponseEntity<String> irAMercadoPago(@PathVariable("cantidad") Integer cantidad, HttpServletRequest request) {
+    public ResponseEntity<String> irAMercadoPago(@PathVariable("cantidad") Integer cantidad) {
+        try {
+            MercadoPagoConfig.setAccessToken("APP_USR-784455809696373-102309-0ae967099b25c21008808523c49a20d5-2050594623");
 
-        Map<String, String> customHeaders = new HashMap<>();
-        customHeaders.put("x-idempotency-key", <SOME_UNIQUE_VALUE>);
+            PreferenceItemRequest itemRequest = PreferenceItemRequest.builder()
+                    .title("Comunico Points")
+                    .quantity(cantidad)
+                    .unitPrice(new BigDecimal(100))
+                    .currencyId("ARS")
+                    .build();
 
-        MPRequestOptions requestOptions = MPRequestOptions.builder()
-                .customHeaders(customHeaders)
-                .build();
+            List<PreferenceItemRequest> items = new ArrayList<>();
+            items.add(itemRequest);
 
-        MercadoPagoConfig.setAccessToken("YOUR_ACCESS_TOKEN");
-
-        PaymentClient client = new PaymentClient();
-
-        PaymentCreateRequest paymentCreateRequest =
-                PaymentCreateRequest.builder()
-                        .transactionAmount(request.getTransactionAmount())
-                        .token(request.getToken())
-                        .description(request.getDescription())
-                        .installments(request.getInstallments())
-                        .paymentMethodId(request.getPaymentMethodId())
-                        .payer(
-                                PaymentPayerRequest.builder()
-                                        .email(request.getPayer().getEmail())
-                                        .firstName(request.getPayer().getFirstName())
-                                        .identification(
-                                                IdentificationRequest.builder()
-                                                        .type(request.getPayer().getIdentification().getType())
-                                                        .number(request.getPayer().getIdentification().getNumber())
-                                                        .build())
-                                        .build())
-                        .build();
-
-        client.create(paymentCreateRequest, requestOptions);
+            PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest
+                    .builder()
+                    .success("http://localhost:8080/comprar?quantity="+cantidad)
+                    .pending("http://localhost:8080/comprar?quantity="+cantidad)
+                    .failure("http://localhost:8080/comprar?quantity="+cantidad)
+                    .build();
 
 
+            PreferenceRequest preferenceRequest = PreferenceRequest.builder()
+                    .items(items)
+                    .backUrls(backUrls)
+                    .build();
+
+            PreferenceClient client = new PreferenceClient();
+            Preference preference = client.create(preferenceRequest);
+
+            return ResponseEntity.ok(preference.getId());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
-    @RequestMapping("/compra/{resultado}")
-    public ModelAndView compra(@PathVariable(value = "resultado", required = false) String resultado){
+    @RequestMapping("/comprar")
+    public ModelAndView compra(@RequestParam Map<String, String> allParams, HttpServletRequest request){
         ModelMap modelo = new ModelMap();
 
-        if(resultado.equals("success"))
-            modelo.put("mensaje", "compra hecha");
-        else if (resultado.equals("pending"))
-            modelo.put("mensaje", "compra pendiente");
-        else if(resultado.equals("failure"))
-            modelo.put("mensaje", "compra fallida");
-
+        String resultado = allParams.get("status");
+        Integer cantidad = Integer.parseInt(allParams.get("quantity"));
+        Long idUsuario = (Long) request.getSession().getAttribute("id");
+        if(resultado != null){
+            if(resultado.equals("approved")){
+                Usuario usuario = this.servicioUsuario.buscarUsuarioPorId(idUsuario);
+                usuario.setComunicoPoints(usuario.getComunicoPoints() + cantidad);
+                this.servicioUsuario.modificar(usuario);
+                modelo.put("resultado", resultado);
+            }
+            else if (resultado.equals("pending"))
+                modelo.put("resultado", resultado);
+            else if(resultado.equals("rejected"))
+                modelo.put("resultado", resultado);
+        }
         return new ModelAndView("mercadopago", modelo);
     }
 }
