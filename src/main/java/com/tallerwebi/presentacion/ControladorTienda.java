55f0b279472ee13eca645409b645d50bf2d9e250
ricartes123 <pedro-le2003@hotmail.com>
2024-11-18 00:38:50 -0300

    Implementa vista Tienda y Llaves en header

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorTienda.java
@@ -0,0 +1,42 @@
+package com.tallerwebi.presentacion;
+
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
+import com.mercadopago.client.preference.PreferenceClient;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.resources.preference.Preference;
+import com.tallerwebi.dominio.ServicioUsuario;
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+public class ControladorTienda {
+
+    private ServicioUsuario servicioUsuario;
+
+    @Autowired
+    public ControladorTienda(ServicioUsuario servicioUsuario) {
+        this.servicioUsuario = servicioUsuario;
+    }
+
+    @RequestMapping("/tienda")
+    public ModelAndView tienda(){
+        ModelMap modelo = new ModelMap();
+
+        return new ModelAndView("tienda", modelo);
+    }
+}
