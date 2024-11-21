b49d8cc4394f707f7b3a21ede5d4455e4b7ec0bd
JulianImperiale <julian.m.imperiale@gmail.com>
2024-11-21 11:18:27 -0300

    Revert "Se agregaron tests para los titulos y la experiencia"
    This reverts commit cdb9bd417c7eef28cd7c15f97383cc504f680733.

--- src/test/java/com/tallerwebi/dominio/ServicioTituloImplTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioTituloImplTest.java
@@ -5,8 +5,6 @@ import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
 
-import java.time.LocalDateTime;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
@@ -16,12 +14,10 @@ public class ServicioTituloImplTest {
     ServicioExperiencia servicioExperienciaMock;
     Usuario usuario;
     Experiencia experiencia;
-    RepositorioUsuario repositorioUsuarioMock;
+    RepositorioUsuario repositorioUsuario;
     RepositorioExperiencia repositorioExperienciaMock;
     ServicioTitulo servicioTitulo;
     RepositorioVida repositorioVidaMock;
-    ServicioVida servicioVida;
-    Vida vidaMock;
 
     private RepositorioProgresoLeccion repositorioProgresoLeccion;
 
@@ -31,25 +27,27 @@ public class ServicioTituloImplTest {
         usuario = new Usuario();
         experiencia = new Experiencia();
 
-        this.repositorioUsuarioMock = mock(RepositorioUsuario.class);
+        this.repositorioUsuario = mock(RepositorioUsuario.class);
         this.repositorioExperienciaMock = mock(RepositorioExperiencia.class);
         this.repositorioVidaMock = mock(RepositorioVida.class);
         this.repositorioProgresoLeccion = mock(RepositorioProgresoLeccion.class);
-        servicioExperienciaMock = new ServicioExperienciaImpl(repositorioUsuarioMock, repositorioExperienciaMock, repositorioProgresoLeccion);
-        servicioTitulo = new ServicioTituloImpl(repositorioUsuarioMock, repositorioVidaMock);
+        servicioExperienciaMock = new ServicioExperienciaImpl(repositorioUsuario, repositorioExperienciaMock, repositorioProgresoLeccion);
+        servicioTitulo = new ServicioTituloImpl(repositorioUsuario, repositorioVidaMock);
         when(repositorioExperienciaMock.buscarExperienciaPorId(experiencia.getId())).thenReturn(experiencia);
-        when(repositorioUsuarioMock.buscarUsuarioPorId(usuario.getId())).thenReturn(usuario);
+        when(repositorioUsuario.buscarUsuarioPorId(usuario.getId())).thenReturn(usuario);
+        //when(usuario.getExperiencia()).thenReturn(experiencia);
         usuario.setExperiencia(experiencia);
 
-    }
+        //when(servicioExperienciaMock.obtenerExperiencia(usuario.getId())).thenReturn(experiencia);
 
+    }
     @Test
     @Rollback
     @Transactional
     public void dadoQueElUsuarioGana100XPTeniendo400XPObtieneElTituloDeNovato() {
         experiencia.setCantidadExperiencia(400);
         this.repositorioExperienciaMock.guardarExperiencia(experiencia);
-        this.repositorioUsuarioMock.guardar(usuario);
+        this.repositorioUsuario.guardar(usuario);
 
         this.servicioExperienciaMock.ganar100DeExperiencia(usuario.getId());
         this.repositorioExperienciaMock.actualizarExperiencia(experiencia);
@@ -60,14 +58,13 @@ public class ServicioTituloImplTest {
 
         assertThat(usuario.getTitulo(), equalTo("Novato"));
     }
-
     @Test
     @Rollback
     @Transactional
     public void dadoQueElUsuarioGana100XPTeniendo4900XPObtieneElTituloDeComunicador() {
         experiencia.setCantidadExperiencia(4900);
         this.repositorioExperienciaMock.guardarExperiencia(experiencia);
-        this.repositorioUsuarioMock.guardar(usuario);
+        this.repositorioUsuario.guardar(usuario);
 
         this.servicioExperienciaMock.ganar100DeExperiencia(usuario.getId());
         this.repositorioExperienciaMock.actualizarExperiencia(experiencia);
@@ -85,27 +82,11 @@ public class ServicioTituloImplTest {
     public void dadoQueElUsuarioExisteYLaExperienciaQueTieneEsMenorA500XPQueElPrimerTituloConElQueVengaSeaConElTituloDePrincipiante() {
         experiencia.setCantidadExperiencia(0);
         this.repositorioExperienciaMock.guardarExperiencia(experiencia);
-        this.repositorioUsuarioMock.guardar(usuario);
+        this.repositorioUsuario.guardar(usuario);
 
         this.servicioTitulo.actualizarTituloSegunExperiencia(usuario.getId());
 
         assertThat(usuario.getTitulo(), equalTo("Principiante"));
-    }
-
-    @Test
-    @Rollback
-    @Transactional
-    public void dadoQueElUsuarioExisteYLaExperienciaQueTieneEsMenorA500XPQueCuandoSupereLos500XpElUsuarioGane20ComunicoPoints() {
-        usuario.setComunicoPoints(0);
-        experiencia.setCantidadExperiencia(400);
-        this.repositorioExperienciaMock.guardarExperiencia(experiencia);
-        this.repositorioUsuarioMock.guardar(usuario);
-        this.servicioExperienciaMock.ganar100DeExperiencia(usuario.getId());
-        this.servicioTitulo.actualizarTituloSegunExperiencia(usuario.getId());
-        this.servicioTitulo.obtenerComunicoPointsCuandoConsigueTitulo(usuario.getId());
-
-        assertThat(usuario.getTitulo(), equalTo("Novato"));
-        assertThat(usuario.getComunicoPoints(), equalTo(20));
     }
 }
 
